b39ad0e598f9df71645b656f667984d0e5b69272
annalast1 <166384111+annalast1@users.noreply.github.comm>
2024-12-04 16:26:21 +0000

    Centered grid

--- js-file.js
+++ js-file.js
@@ -2,7 +2,7 @@ let body = document.getElementById("body");
 
 function createCols() {
 // Create 16 divs and append to container div - making 16 cols
-    for (let cols = 0; cols < 6; cols++) {
+    for (let cols = 0; cols < 16; cols++) {
         let col = document.createElement("section");
         body.appendChild(col);
         col.setAttribute("id", "col" + cols);
@@ -13,8 +13,8 @@ createCols();
 
 function createPixel() {
     let x = 0;
-    while (x < 6) {
-        for (let i = 0; i < 6; i++) {
+    while (x < 16) {
+        for (let i = 0; i < 16; i++) {
             let pixel = document.createElement("div");
             let col = document.getElementById("col" + i);
             col.appendChild(pixel);
