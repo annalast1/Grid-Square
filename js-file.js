d4fcb4639238f8b768042464dffed3cb7621b18f
annalast1 <166384111+annalast1@users.noreply.github.comm>
2024-12-05 19:40:54 +0000

    Completed black and white version

--- js-file.js
+++ js-file.js
@@ -1,8 +1,9 @@
+let size = prompt("Choose the size of grid (Max : 100)")
 let body = document.getElementById("body");
 
 function createCols() {
 // Create 16 divs and append to container div - making 16 cols
-    for (let cols = 0; cols < 16; cols++) {
+    for (let cols = 0; cols < size; cols++) {
         let col = document.createElement("section");
         body.appendChild(col);
         col.setAttribute("id", "col" + cols);
@@ -13,12 +14,15 @@ createCols();
 
 function createPixel() {
     let x = 0;
-    while (x < 16) {
-        for (let i = 0; i < 16; i++) {
+    while (x < size) {
+        for (let i = 0; i < size; i++) {
             let pixel = document.createElement("div");
             let col = document.getElementById("col" + i);
             col.appendChild(pixel);
             pixel.setAttribute("class", "row" + x);
+            pixel.addEventListener("mouseover", () => {
+                pixel.style.backgroundColor = 'black'
+            })
         }
         x++;
     }
